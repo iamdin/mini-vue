2a66dfc8800c1eb1238b4172df7d0dd60efeca1c
jinyang <djy.dingjinyang@gmail.com>
2022-03-14 16:36:10 +0800

    feat(effect): stop

--- src/reactivity/tests/effect.spec.ts
+++ src/reactivity/tests/effect.spec.ts
@@ -1,5 +1,5 @@
 import { reactive } from '../reactive'
-import { effect } from '../effect'
+import { effect, stop } from '../effect'
 describe('reactivity', () => {
   it('happy path', () => {
     const user = reactive({
@@ -55,7 +55,7 @@ describe('reactivity', () => {
       { scheduler }
     )
 
-    expect(scheduler).not.toHaveBeenCalled();
+    expect(scheduler).not.toHaveBeenCalled()
     expect(dummy).toBe(1)
 
     // should be called on first trigger
@@ -68,5 +68,41 @@ describe('reactivity', () => {
     run()
     // should have run
     expect(dummy).toBe(2)
+  })
+
+  it('stop', () => {
+    // 停止响应式触发
+    let dummy
+    const obj = reactive({ prop: 1 })
+    const runner = effect(() => {
+      dummy = obj.prop
+    })
+    obj.prop = 2
+    expect(dummy).toBe(2)
+    stop(runner)
+    obj.prop = 3
+    expect(dummy).toBe(2)
+
+    // stopped effect should still be manually callable
+    runner()
+    expect(dummy).toBe(3)
+  })
+
+  it('onStop', () => {
+    // onStop 回调函数
+    const obj = reactive({
+      foo: 1,
+    })
+    const onStop = jest.fn()
+    let dummy
+    const runner = effect(
+      () => {
+        dummy = obj.foo
+      },
+      { onStop }
+    )
+
+    stop(runner)
+    expect(onStop).toBeCalledTimes(1)
   })
 })
