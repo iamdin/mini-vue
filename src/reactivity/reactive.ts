8d54231cb20adbc1c18263482fbb61ab8377bd1e
jinyang <djy.dingjinyang@gmail.com>
2022-03-15 19:45:50 +0800

    refactor(reactive): refact reactive & readonly with basehandler

--- src/reactivity/reactive.ts
+++ src/reactivity/reactive.ts
@@ -1,34 +1,13 @@
-import { track, trigger } from './effect'
+import { mutableHandlers, readonlyHandlers } from './baseHandlers'
 
-export function reactive(raw) {
-  return new Proxy(raw, {
-    get(target, key) {
-      const res = Reflect.get(target, key)
-
-      // 收集依赖
-      track(target, key)
-
-      return res
-    },
-    set(target, key, value) {
-      const res = Reflect.set(target, key, value)
-
-      // 触发依赖
-      trigger(target, key)
-
-      return res
-    },
-  })
+export function reactive(raw: any) {
+  return createActiveObject(raw, mutableHandlers)
 }
 
-export function readonly(raw) {
-  return new Proxy(raw, {
-    get(target, key) {
-      return Reflect.get(target, key)
-    },
-    set(target, key) {
-      console.warn(`key: ${String(key)} set failed, target ${target} is readonly`)
-      return true
-    },
-  })
+export function readonly(raw: any) {
+  return createActiveObject(raw, readonlyHandlers)
+}
+
+function createActiveObject(raw: any, baseHandlers: ProxyHandler<any>) {
+  return new Proxy(raw, baseHandlers)
 }
