a218eeda1ce73a7a45574d8bf599e9096f57dfa2
jinyang <djy.dingjinyang@gmail.com>
2022-03-16 09:54:01 +0800

    feat(ref): isRef & unRef

--- src/reactivity/tests/ref.spec.ts
+++ src/reactivity/tests/ref.spec.ts
@@ -1,5 +1,6 @@
 import { effect } from '../effect'
-import { ref } from '../ref'
+import { reactive } from '../reactive'
+import { isRef, ref, unRef } from '../ref'
 
 describe('ref', () => {
   it('happy path', () => {
@@ -43,5 +44,24 @@ describe('ref', () => {
 
     a.value.count = 2
     expect(dummy).toBe(2)
+  })
+
+  it('isRef', () => {
+    const a = ref(1)
+    const user = reactive({ age: 1 })
+
+    expect(isRef(undefined)).toBe(false)
+    expect(isRef(null)).toBe(false)
+    expect(isRef(a)).toBe(true)
+    expect(isRef(1)).toBe(false)
+    expect(isRef(user)).toBe(false)
+  })
+
+  it('unRef', () => {
+    const a = ref(1)
+    const user = reactive({ age: 1 })
+
+    expect(unRef(a)).toBe(1)
+    expect(unRef(1)).toBe(1)
   })
 })
