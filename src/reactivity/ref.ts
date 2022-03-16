1fd9418f0827f3fb2c916583af95db8c6bf67662
jinyang <djy.dingjinyang@gmail.com>
2022-03-16 10:29:01 +0800

    feat(ref): proxyRefs

--- src/reactivity/ref.ts
+++ src/reactivity/ref.ts
@@ -48,3 +48,20 @@ export function isRef(ref) {
 export function unRef(ref) {
   return isRef(ref) ? ref.value : ref
 }
+
+export function proxyRefs(objectWithRefs) {
+  return new Proxy(objectWithRefs, {
+    get(target, key) {
+      return unRef(Reflect.get(target, key))
+    },
+
+    set(target, key, value) {
+      // key is ref, value not ref, operate the value
+      if (isRef(target[key]) && !isRef(value)) {
+        return (target[key].value = value)
+      } else {
+        return Reflect.set(target, key, value)
+      }
+    },
+  })
+}
